04a444d7d562cf11f8da6f0f88a273f8b8a14be4
hobathanh <hobathanh201@gmail.com>
2023-03-01 14:47:11 +0700

    set up project

---
+++ src/test/java/com/bathanh/apibook/ApiBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bathanh.apibook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
