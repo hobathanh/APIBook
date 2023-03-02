b0cfbd2aa5eed4c12f68a4adb5104fee285026b1
hobathanh <hobathanh201@gmail.com>
2023-03-02 16:51:19 +0700

    fix hardcode userid

--- src/main/resources/db/migration/COMMON/V1_0_0_020320231323__create_table_users.sql
+++ src/main/resources/db/migration/COMMON/V1_0_0_020320231323__create_table_users.sql
@@ -13,8 +13,5 @@ CREATE TABLE users
     FOREIGN KEY (role_id) REFERENCES roles(id)
 );
 
-INSERT INTO users (username, password, firstName, lastName, enabled, avatar, role_id)
-VALUES ('admin', '123456', 'admin', 'admin', true, 'avatar1.jpg', (SELECT id FROM roles WHERE name = 'ADMIN'));
-
-INSERT INTO users (username, password, firstName, lastName, enabled, avatar, role_id)
-VALUES ('thanh', '123456', 'ba', 'thanh', true, 'avatar2.jpg', (SELECT id FROM roles WHERE name = 'CONTRIBUTOR'));
+INSERT INTO users (id, username, password, firstName, lastName, enabled, avatar, role_id)
+VALUES ('6302a41b-e61f-40a9-9dd0-9eb10028a793', 'admin', '123456', 'admin', 'admin', true, 'avatar1.jpg', (SELECT id FROM roles WHERE name = 'ADMIN'));
--- src/main/resources/db/migration/COMMON/V1_0_0_020320231324__create_table_books.sql
+++ src/main/resources/db/migration/COMMON/V1_0_0_020320231324__create_table_books.sql
@@ -12,10 +12,3 @@ CREATE TABLE books
       user_id UUID NOT NULL,
       FOREIGN KEY (user_id) REFERENCES users(id)
 );
-
-INSERT INTO books (title, author, description, created_at, updated_at, image, user_id)
-VALUES ('Harry Potter', 'J. K. Rowling', 'Harry Potter is a seven-part series of paranormal novels by English writer J. K. Rowling',
-        '2023-03-02 12:00:00', '2023-03-02 12:00:00', 'image_Book1.jpg', (SELECT id FROM users WHERE username = 'thanh'));
-INSERT INTO books (title, author, description, created_at, updated_at, image, user_id)
-VALUES ('Harry Potter2', 'J. K. Rowling', 'Writer J. K. Rowling',
-        '2023-03-02 12:02:00', '2023-03-02 12:02:00', 'image_Book2.jpg', (SELECT id FROM users WHERE username = 'thanh'));
