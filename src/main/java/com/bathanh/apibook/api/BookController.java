6e99750d17754026242e93a5d601bcd60aa9a775
hobathanh <hobathanh201@gmail.com>
2023-03-02 09:57:53 +0700

    add api documentaion

--- src/main/java/com/bathanh/apibook/api/BookController.java
+++ src/main/java/com/bathanh/apibook/api/BookController.java
@@ -3,6 +3,7 @@ package com.bathanh.apibook.api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.awt.print.Book;
 import java.util.List;
 
---
+++ src/main/java/com/bathanh/apibook/configuration/ApiDocumentationConfiguration.java
@@ -0,0 +1,29 @@
+package com.bathanh.apibook.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Book Store API",
+                version = "v1",
+                contact = @Contact(
+                        name = "Ho Ba Thanh",
+                        email = "hobathanh201@gmail.com"
+                )
+        ),
+        security = @SecurityRequirement(name = "bearerAuth"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class ApiDocumentationConfiguration {
+}
