bc9be2f16a312fcb517e561ba28409d22df54493
hobathanh <hobathanh201@gmail.com>
2023-04-04 13:00:37 +0700

    fix codecov

--- src/test/java/com/bathanh/apibook/fakes/JwtFakes.java
+++ src/test/java/com/bathanh/apibook/fakes/JwtFakes.java
@@ -8,10 +8,18 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import java.util.HashSet;
 import java.util.Set;
 
+import static java.util.UUID.randomUUID;
+
 @UtilityClass
 public class JwtFakes {
 
     public static JwtUserDetails buildJwtUserDetails() {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        return new JwtUserDetails(randomUUID(), "testUser", "testPassword", authorities);
+    }
+
+    public static JwtUserDetails buildJwtUserDetail_NullUserId() {
         Set<GrantedAuthority> authorities = new HashSet<>();
         authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
         return new JwtUserDetails(null, "testUser", "testPassword", authorities);
