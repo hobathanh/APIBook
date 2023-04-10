16fcb4463f1d02b6ca44cf837a70123371b5c948
hobathanh <hobathanh201@gmail.com>
2023-04-10 16:47:57 +0700

    fix covde cov

--- src/main/java/com/bathanh/apibook/domain/user/SocialUser.java
+++ src/main/java/com/bathanh/apibook/domain/user/SocialUser.java
@@ -2,11 +2,9 @@ package com.bathanh.apibook.domain.user;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Builder
 @Getter
-@Setter
 public class SocialUser {
 
     private String firstName;
