d2c736b2153d9e426ce00947d0fd602e78f205e7
hobathanh <hobathanh201@gmail.com>
2023-04-11 17:28:36 +0700

    fix codecov

---
+++ src/test/java/com/bathanh/apibook/domain/book/CloudinaryServiceTest.java
@@ -0,0 +1,46 @@
+package com.bathanh.apibook.domain.book;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.Uploader;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CloudinaryServiceTest {
+
+    @InjectMocks
+    private CloudinaryService cloudinaryService;
+
+    @Mock
+    private Cloudinary cloudinary;
+
+    @BeforeEach
+    void setUp() {
+        when(cloudinary.uploader()).thenReturn(mock(Uploader.class));
+        cloudinaryService = new CloudinaryService(cloudinary);
+    }
+
+    @Test
+    void shouldUploadImage_Successfully() throws IOException {
+        final var bytes = new byte[]{0x12, 0x34, 0x56, 0x78};
+
+        final var cloudinaryResponse = Map.of("secure_url", "https://res.cloudinary.com/test/image/upload/test.jpg");
+        when(cloudinary.uploader().upload(any(byte[].class), any())).thenReturn(cloudinaryResponse);
+
+        final var result = cloudinaryService.upload(bytes);
+
+        assertEquals(cloudinaryResponse.get("secure_url"), result);
+    }
+}
