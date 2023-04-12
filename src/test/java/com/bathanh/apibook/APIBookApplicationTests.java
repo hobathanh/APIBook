957ce795639477adc4580924d2c8e00ed8d487ab
hobathanh <hobathanh201@gmail.com>
2023-04-12 13:10:37 +0700

    fix CI

--- src/test/java/com/bathanh/apibook/APIBookApplicationTests.java
+++ src/test/java/com/bathanh/apibook/APIBookApplicationTests.java
@@ -3,7 +3,7 @@ package com.bathanh.apibook;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = APIBookApplicationTests.class)
 class APIBookApplicationTests {
 
     @Test
